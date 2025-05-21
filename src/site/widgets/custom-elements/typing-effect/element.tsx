9a841127950ba4641678fb8026dd69c421533809
ThaveeshaSonnadara-MKP <thaveesha@marketpushapps.com>
2025-05-21 16:31:45 +0530

    Removed renderTextElement function, deleted use memo hook for typedWords array generation, and added getDynamicFontWeight & getStaticFontWeight

--- src/site/widgets/custom-elements/typing-effect/element.tsx
+++ src/site/widgets/custom-elements/typing-effect/element.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState, type FC } from "react";
+import React, { useCallback, useEffect, useState, type FC } from "react";
 import ReactDOM from "react-dom";
 import reactToWebComponent from "react-to-webcomponent";
 import GraphemeSplitter from "grapheme-splitter";
@@ -6,7 +6,7 @@ import "@wix/design-system/styles.global.css";
 import { Typewriter } from "react-simple-typewriter";
 import { WidgetData } from "../../../../types/common-types";
 import { DEFAULT_WIDGET_DATA } from "../../../../Utils/constants";
-import { getHtmlTag } from "../../../../Utils/tools";
+// import { getHtmlTag } from "../../../../Utils/tools";
 
 const SPLITTER = new GraphemeSplitter();
 
@@ -24,7 +24,7 @@ const TypingEffectWidget: FC<Props> = ({ widgetData }) => {
 
   // USE_EFFECTS
   useEffect(() => {
-    console.log("Fetching widget data...");
+    // console.log("Fetching widget data...");
 
     if (!widgetData) return;
 
@@ -33,18 +33,31 @@ const TypingEffectWidget: FC<Props> = ({ widgetData }) => {
       if (parsed) {
         setWidgetDataState(parsed);
 
-        console.log("Updated widget data:", parsed);
-        console.log("Widget data fetched successfully.");
+        // console.log("Updated widget data:", parsed);
+        // console.log("Widget data fetched successfully.");
       }
     } catch (e) {
       console.error("Error in widget-data fetch.", e);
     }
   }, [widgetData]);
 
-  const typedWords = useMemo(() => {
+  const getDynamicFontWeight = () => {
+    const rawDynamicFontWeight =
+      widgetDataState.dynamicText.fontWeight.toLowerCase();
+    if (rawDynamicFontWeight === "bolder") return 900;
+    return rawDynamicFontWeight;
+  };
+  const getStaticFontWeight = () => {
+    const rawStaticFontWeight =
+      widgetDataState.staticText.fontWeight.toLowerCase();
+    if (rawStaticFontWeight === "bolder") return 900;
+    return rawStaticFontWeight;
+  };
+
+  const typedWords = () => {
     const texts = widgetDataState.dynamicText.texts || [""];
     return texts.map((t) => SPLITTER.splitGraphemes(t).join(""));
-  }, [widgetDataState.dynamicText.texts]);
+  };
 
   function getValidTextAlign(value?: string): TextAlign {
     const val = value?.toLowerCase();
@@ -53,47 +66,13 @@ const TypingEffectWidget: FC<Props> = ({ widgetData }) => {
       : "center";
   }
 
-  function renderTextElement(tagName: string) {
-    const Tag = tagName as keyof JSX.IntrinsicElements;
-
-    return (
-      <Tag
-        style={{
-          color: widgetDataState.staticText.fontColor,
-          fontWeight: widgetDataState.staticText.fontWeight.toLowerCase(),
-          fontSize: widgetDataState.staticText.fontSize,
-        }}
-      >
-        {widgetDataState.staticText.text}
-        <span
-          style={{
-            color: widgetDataState.dynamicText.fontColor,
-            fontWeight: widgetDataState.dynamicText.fontWeight.toLowerCase(),
-            fontSize: widgetDataState.dynamicText.fontSize,
-            marginLeft: widgetDataState.staticText.text
-              ? widgetDataState.textsGap
-              : 0,
-          }}
-        >
-          <Typewriter
-            words={typedWords}
-            loop={widgetDataState.animationOptions?.loop ?? 0}
-            cursor={widgetDataState.animationOptions?.cursor ?? true}
-            cursorStyle={widgetDataState.animationOptions?.cursorStyle ?? "|"}
-            typeSpeed={widgetDataState.animationOptions?.typeSpeed ?? 70}
-            deleteSpeed={widgetDataState.animationOptions?.deleteSpeed ?? 50}
-            delaySpeed={widgetDataState.animationOptions?.delaySpeed ?? 1000}
-          />
-        </span>
-      </Tag>
-    );
-  }
-
   return (
     <div
       style={{
         width: "100%",
         height: "100%",
+        minHeight: "50px",
+        minWidth: "245px",
         backgroundColor: `${widgetDataState?.backgroundColor || "transparent"}`,
         display: `${widgetDataState?.display?.toLowerCase() || "flex"}`,
         alignItems: `${widgetDataState?.alignItems?.toLowerCase() || "center"}`,
@@ -103,7 +82,35 @@ const TypingEffectWidget: FC<Props> = ({ widgetData }) => {
         textAlign: getValidTextAlign(widgetDataState?.textAlignProp),
       }}
     >
-      {renderTextElement(getHtmlTag(widgetDataState?.textElementType))}
+      <span
+        style={{
+          color: widgetDataState.staticText.fontColor,
+          fontWeight: getStaticFontWeight(),
+          fontSize: widgetDataState.staticText.fontSize,
+        }}
+      >
+        {widgetDataState.staticText.text}
+        <span
+          style={{
+            color: widgetDataState.dynamicText.fontColor,
+            fontWeight: getDynamicFontWeight(),
+            fontSize: widgetDataState.dynamicText.fontSize,
+            marginLeft: widgetDataState.staticText.text
+              ? widgetDataState.textsGap
+              : 0,
+          }}
+        >
+          <Typewriter
+            words={typedWords()}
+            loop={widgetDataState.animationOptions?.loop ?? 0}
+            cursor={widgetDataState.animationOptions?.cursor ?? true}
+            cursorStyle={widgetDataState.animationOptions?.cursorStyle ?? "|"}
+            typeSpeed={widgetDataState.animationOptions?.typeSpeed ?? 70}
+            deleteSpeed={widgetDataState.animationOptions?.deleteSpeed ?? 50}
+            delaySpeed={widgetDataState.animationOptions?.delaySpeed ?? 1000}
+          />
+        </span>
+      </span>
     </div>
   );
 };
