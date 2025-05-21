c17d7c0b1204f5a5a94fa8c154b2cd3d438b0428
ThaveeshaSonnadara-MKP <thaveesha@marketpushapps.com>
2025-05-21 16:28:09 +0530

    Removed unnecessary font weight options & kept only 3 options in the fontWeightOptions constant

--- src/Utils/get-css-properties.ts
+++ src/Utils/get-css-properties.ts
@@ -1,22 +1,7 @@
 const fontWeightOptions = [
-    { id: 'fw-1', value: '100' },
-    { id: 'fw-2', value: '200' },
-    { id: 'fw-3', value: '300' },
-    { id: 'fw-4', value: '400' },
-    { id: 'fw-5', value: '500' },
-    { id: 'fw-6', value: '600' },
-    { id: 'fw-7', value: '700' },
-    { id: 'fw-8', value: '800' },
-    { id: 'fw-9', value: '900' },
-    { id: 'fw-10', value: 'Normal' },
-    { id: 'fw-11', value: 'Bold' },
-    { id: 'fw-12', value: 'Lighter' },
-    { id: 'fw-13', value: 'Bolder' },
-    { id: 'fw-14', value: 'Inherit' },
-    { id: 'fw-15', value: 'Initial' },
-    { id: 'fw-16', value: 'Revert' },
-    { id: 'fw-17', value: 'Revert-layer' },
-    { id: 'fw-18', value: 'Unset' }
+    { id: 'fw-1', value: 'Normal' },
+    { id: 'fw-2', value: 'Bold' },
+    { id: 'fw-3', value: 'Bolder' }
 ];
 
 export const getCssPropertyValuesAvailable = (property: string) => {
